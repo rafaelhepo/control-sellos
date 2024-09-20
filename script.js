76cd0820a5de69783a0ec8fe8ae57a7546c38956
luisflores93 <jose.flores.contractor@realtor.com>
2024-09-20 10:55:33 -0600

    Funcionalidad a cada uno de los inputs

--- script.js
+++ script.js
@@ -15,9 +15,59 @@
             fechaInput.max = fechaActual;
         });
 
+// Función para capturar los valores de Saldo y Dotación y realizar operaciones
+function calcular() {
+    // Obtener los valores de los campos "Saldo" y "Dotación"
+    let saldo = parseFloat(document.getElementById('saldo').value);
+    let dotacion = parseFloat(document.getElementById('dotacion').value);
 
 
-/*document.querySelector('button[type="submit"]').addEventListener('click', function(event) {
-    event.preventDefault(); // Evita que el formulario se envíe automáticamente
-    alert('Te deben quedar 100 sellos utilizables');
-});*/
+    // Verificar si los valores son válidos
+    if (isNaN(saldo) || isNaN(dotacion)) {
+        alert('Por favor, ingrese valores válidos en Saldo y Dotación.');
+        return;
+    }
+
+    var resultado = saldo + dotacion;
+
+
+    let cargasPlata = parseFloat(document.getElementById('cargas-np300-plata').value);
+    let quedaronPlata = parseFloat(document.getElementById('ultimos-np300-plata').value);
+
+    let cargasBlanca = parseFloat(document.getElementById('cargas-np300-blanca').value);
+    let quedaronBlanca = parseFloat(document.getElementById('ultimos-np300-blanca').value);
+
+    let cargasGris = parseFloat(document.getElementById('cargas-gris').value);
+    let quedaronGris = parseFloat(document.getElementById('ultimos-gris').value);
+
+    if (isNaN(cargasPlata)) {
+        alert('Por favor, ingrese un valor válido para las cargas de la camioneta NP 300 PLATA.');
+        return;
+    } else if (isNaN(quedaronPlata)) {
+        alert('Por favor, ingrese un valor válido para los últimos que quedaron de la camioneta NP 300 PLATA.');
+        return;
+    } else if (isNaN(cargasBlanca)) {
+        alert('Por favor, ingrese un valor válido para las cargas de la camioneta NP 300 BLANCA.');
+        return;
+    } else if (isNaN(quedaronBlanca)) {
+        alert('Por favor, ingrese un valor válido para los últimos que quedaron de la camioneta NP 300 BLANCA.');
+        return;
+    } else if (isNaN(cargasGris)) {
+        alert('Por favor, ingrese un valor válido para las cargas de la camioneta GRIS.');
+        return;
+    } else if (isNaN(quedaronGris)) {
+        alert('Por favor, ingrese un valor válido para los últimos que quedaron de la camioneta GRIS.');
+        return;
+    };
+
+    
+    alert('Todos los valores son válidos. Procediendo con la operación.');
+    
+    
+};
+
+
+
+
+
+
